07101fb72018d89b6093b79362aeeada3579905e
serhanelmali <serhanelmali@gmail.com>
2021-07-05 14:48:07 +0300

    create html and css structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator__wrapper">
+        <div className="calculator__screen">
+          <span>0</span>
+        </div>
+        <div className="calculator__buttons">
+          <div className="calculator__buttons__numbers">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+            <button id="clear">clear</button>
+          </div>
+          <div className="calculator__buttons__functions">
+            <button>÷</button>
+            <button>-</button>
+            <button>+</button>
+            <button>=</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
